7d295f188cb92a189a8b99d3fa63a08a6f4c0236
Kanav Anand <kanand@insy-login.hpc.tudelft.nl>
2019-04-12 14:01:26 +0200

    adding job script

---
+++ le-net/run_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --partition=general
+#SBATCH --qos=short
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4096
+#SBATCH --mail-type=END
+#SBATCH --gres=gpu
+module use /opt/insy/modulefiles
+srun python3 < main.py 
