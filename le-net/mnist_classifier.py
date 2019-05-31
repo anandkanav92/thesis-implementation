7c384bcd0f3fc4b3bfe7fcc9aeaf51c53c938103
anandkanav92 <anandkanav92@gmail.com>
2019-05-31 15:54:55 +0200

    try densenet

--- le-net/mnist_classifier.py
+++ le-net/mnist_classifier.py
@@ -68,8 +68,9 @@ class Black_Magic():
     self.params = params
     #logging.debug(self.use_cuda)
     # self.model = LeNet5()
-    self.model = squeezenet1_1(pretrained=False,num_classes=10)
-    # logging.debug(summary(self.model, (3, 128, 128)))
+    # self.model = squeezenet1_1(pretrained=False,num_classes=10)
+    self.model = tmodel.densenet121(pretrained=False,num_classes=10)
+    # logging.debug(summary(self.model, (3, 224, 224)))
 
     self.viz = visdom.Visdom()
     self.push_to_viz = True
@@ -87,7 +88,7 @@ class Black_Magic():
   def read_data_imagenette(self):
     data_train = Imagenette('./data/imagenette',
                        transform=transforms.Compose([
-                           transforms.Resize((128, 128)),
+                           transforms.Resize((224, 224)),
                            transforms.ToTensor(),
                            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                            ]))
@@ -95,7 +96,7 @@ class Black_Magic():
     data_test = Imagenette('./data/imagenette',
                       train=False,
                       transform=transforms.Compose([
-                          transforms.Resize((128, 128)),
+                          transforms.Resize((224, 224)),
                           transforms.ToTensor(),
                           transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]))
     data_train_loader = DataLoader(data_train, batch_size=int(self.params[Constants.BATCH_SIZE][Constants.VALUE]), shuffle=True, num_workers=8)
